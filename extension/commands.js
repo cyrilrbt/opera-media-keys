595c0237703f68eb344007ab452dd9a5d93ce143
Cyril <cyril@hippie.io>
2017-08-02 11:36:12 -0400

    Small refactor

--- extension/commands.js
+++ extension/commands.js
@@ -1,27 +1,15 @@
 chrome.commands.onCommand.addListener(function(command){
-	if (command == "media_playpause") {
-        chrome.tabs.query({url: '*://www.youtube.com/*'}, function(tabs) {
-            tabs.forEach(function(tab) {
-                chrome.tabs.executeScript(tab.id, {file: 'jquery-3.2.1.min.js'}, function() {
+    chrome.tabs.query({url: '*://www.youtube.com/*'}, function(tabs) {
+        tabs.forEach(function(tab) {
+            chrome.tabs.executeScript(tab.id, {file: 'jquery-3.2.1.min.js'}, function() {
+                if (command == 'media_playpause') {
                     chrome.tabs.executeScript(tab.id, {file: 'youtube/playpause.js'});
-                });
-            });
-        });
-	} else if (command == 'media_next') {
-        chrome.tabs.query({url: '*://www.youtube.com/*'}, function(tabs) {
-            tabs.forEach(function(tab) {
-                chrome.tabs.executeScript(tab.id, {file: 'jquery-3.2.1.min.js'}, function() {
+                } else if (command == 'media_next') {
                     chrome.tabs.executeScript(tab.id, {file: 'youtube/next.js'});
-                });
-            });
-        });
-    } else if (command == 'media_prev') {
-        chrome.tabs.query({url: '*://www.youtube.com/*'}, function(tabs) {
-            tabs.forEach(function(tab) {
-                chrome.tabs.executeScript(tab.id, {file: 'jquery-3.2.1.min.js'}, function() {
+                } else if (command == 'media_prev') {
                     chrome.tabs.executeScript(tab.id, {file: 'youtube/prev.js'});
-                });
+                }
             });
         });
-    }
+    });
 });
